eaa339382eb8f8e2c7ca67f4184feb365504a6e4
Rana Debnath <ranadebnath619@gmail.com>
2021-11-04 18:51:16 +0530

    cors modification

--- src/server.js
+++ src/server.js
@@ -46,11 +46,14 @@ ConnectMongoDB();
 
 
 //middlewear
-app.use(cors());
+app.use(cors({
+    origin:'*', 
+    credentials:true,        
+}));
+
 app.use(express.json());
 
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
-
 app.use('/api', authRoutes)
 app.use('/api', adminRoutes)
 app.use('/api', categoryRoutes)
